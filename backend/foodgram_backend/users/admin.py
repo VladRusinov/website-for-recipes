b31db9f91dcbb815e73fada6c9e3e3e49fce1fe5
Rusinov Vlad <vladrusinov1104@gmail.com>
2024-04-14 23:49:23 +0300

    fix users.admin

--- backend/foodgram_backend/users/admin.py
+++ backend/foodgram_backend/users/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 from users.models import Follow, User
 
 
 @admin.register(User)
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     list_display = (
         'username',
         'email',

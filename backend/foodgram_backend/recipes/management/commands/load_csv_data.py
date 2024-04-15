a91754856267713d483d6caecb5927d9dd5b2baa
Rusinov Vlad <vladrusinov1104@gmail.com>
2024-04-15 23:32:11 +0300

    fix load_csv_data.py

--- backend/foodgram_backend/recipes/management/commands/load_csv_data.py
+++ backend/foodgram_backend/recipes/management/commands/load_csv_data.py
@@ -1,5 +1,6 @@
 import csv
 from django.core.management import BaseCommand
+from tqdm import tqdm
 
 from recipes.models import Ingredient, Tag
 
@@ -8,17 +9,21 @@ class Command(BaseCommand):
     help = "Loads data from csv files"
 
     def handle(self, *args, **options):
-        print("Loading data")
-        for row in csv.reader(
-            open('data/ingredients.csv', encoding='utf-8')
+        self.stdout.write("loading data", ending='')
+        for row in tqdm(
+            (csv.reader(open(
+                'data/ingredients.csv', encoding='utf-8'
+            ))), ncols=80, ascii=True, desc='Total'
         ):
             ingredient = Ingredient(
                 name=row[0], measurement_unit=row[1]
             )
             ingredient.save()
 
-        for row in csv.reader(
-            open('data/tags.csv', encoding='utf-8')
+        for row in tqdm(
+            (csv.reader(open(
+                'data/tags.csv', encoding='utf-8'
+            ))), ncols=80, ascii=True, desc='Total'
         ):
             tag = Tag(
                 name=row[0], color=row[1], slug=row[2]

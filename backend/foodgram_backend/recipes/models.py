9862d413b33ddb3107b07e4c7a13fcfd0fdd7d64
Rusinov Vlad <vladrusinov1104@gmail.com>
2024-04-16 00:39:56 +0300

    fix

--- backend/foodgram_backend/recipes/models.py
+++ backend/foodgram_backend/recipes/models.py
@@ -43,7 +43,7 @@ class Ingredient(models.Model):
         verbose_name_plural = 'Ингредиенты'
         constraints = [
             models.UniqueConstraint(
-                fields=['name', ' measurement_unit'], name='unique_measurement'
+                fields=['name', 'measurement_unit'], name='unique_measurement'
             ),
         ]
 

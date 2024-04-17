e0685417e2aaf469407978538eb3765fe9e706fb
Rusinov Vlad <vladrusinov1104@gmail.com>
2024-04-17 23:45:49 +0300

    fix

--- backend/foodgram_backend/users/serializers.py
+++ backend/foodgram_backend/users/serializers.py
@@ -115,11 +115,11 @@ class SubscribeSerializer(serializers.ModelSerializer):
 class SubscriptionSerializer(UserSerializer):
     """Сериализатор подписок."""
 
-    email = serializers.ReadOnlyField(source='following.email') 
-    id = serializers.ReadOnlyField(source='following.id') 
-    username = serializers.ReadOnlyField(source='following.username') 
-    first_name = serializers.ReadOnlyField(source='following.first_name') 
-    last_name = serializers.ReadOnlyField(source='following.last_name') 
+    email = serializers.ReadOnlyField(source='following.email')
+    id = serializers.ReadOnlyField(source='following.id')
+    username = serializers.ReadOnlyField(source='following.username')
+    first_name = serializers.ReadOnlyField(source='following.first_name')
+    last_name = serializers.ReadOnlyField(source='following.last_name')
     is_subscribed = serializers.SerializerMethodField()
     recipes = serializers.SerializerMethodField()
     recipes_count = serializers.SerializerMethodField()

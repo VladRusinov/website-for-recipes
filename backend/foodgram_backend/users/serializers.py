daffc75a3a332b296233a784c7e10c901cbf079e
Rusinov Vlad <vladrusinov1104@gmail.com>
2024-04-17 18:23:48 +0300

    fix

--- backend/foodgram_backend/users/serializers.py
+++ backend/foodgram_backend/users/serializers.py
@@ -97,7 +97,7 @@ class SubscribeSerializer(serializers.ModelSerializer):
         request = self.context.get('request')
         context = {'request': request}
         serializer = SubscriptionSerializer(
-            instance,
+            request.user,
             context=context
         )
         return serializer.data

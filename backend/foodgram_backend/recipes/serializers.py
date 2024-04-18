144ebb8fee292fb299e1922aef52b0a74b6b7d89
Rusinov Vlad <vladrusinov1104@gmail.com>
2024-04-18 09:05:51 +0300

    fix

--- backend/foodgram_backend/recipes/serializers.py
+++ backend/foodgram_backend/recipes/serializers.py
@@ -179,6 +179,14 @@ class FavoriteSerializer(serializers.ModelSerializer):
             'request': self.context.get('request')
         }).data
 
+    def validate(self, data):
+        user = data['user']
+        if user.favorites.filter(recipe=data['recipe']).exists():
+            raise serializers.ValidationError(
+                'Рецепт уже добавлен в избранное'
+            )
+        return data
+
 
 class ShoppingCartSerializer(serializers.ModelSerializer):
     """Сериализатор модели ShoppingCart."""
@@ -191,3 +199,11 @@ class ShoppingCartSerializer(serializers.ModelSerializer):
         return RecipeForFollowSerializer(instance.recipe, context={
             'request': self.context.get('request')
         }).data
+
+    def validate(self, data):
+        user = data['user']
+        if user.shopping_carts.filter(recipe=data['recipe']).exists():
+            raise serializers.ValidationError(
+                'Рецепт уже добавлен в список покупок'
+            )
+        return data
